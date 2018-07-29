318972080c367ab316e58457224b6264c21a3cb7
Bohdan <bohdan.boiko@gmail.com>
2018-07-29 13:28:20 +0300

    Settings for gridster

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [310, 310]
+        widget_base_dimensions: [310, 310],
+        max_cols: 6,
+        max_rows: 6,
+        resize: {
+            enabled: true
+        }
     });
 });
